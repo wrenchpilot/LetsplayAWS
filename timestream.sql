59074a110001a2c645e9181096028642da04e365
James Shawn Carnley <shawn@floatingboy.org>
2020-12-13 21:53:52 -0500

    update query to match new schema

--- timestream.sql
+++ timestream.sql
@@ -1,9 +1,14 @@
-SELECT time, 
-      MAX(CASE WHEN measure_name = 'gyro' THEN measure_value::varchar END) "gyroXYZ",
-      MAX(CASE WHEN measure_name = 'acc' THEN measure_value::varchar END) "accXYZ",
-      MAX(CASE WHEN measure_name = 'ahrs' THEN measure_value::varchar END) "ahrsPRY"
-FROM "IMU"."data" 
-WHERE measure_name != 'sensor' 
+SELECT time,
+     MAX(CASE WHEN measure_name = 'gyroX' THEN measure_value::double END) "gyroX",
+     MAX(CASE WHEN measure_name = 'gyroY' THEN measure_value::double END) "gyroY",
+     MAX(CASE WHEN measure_name = 'gyroZ' THEN measure_value::double END) "gyroZ",
+     MAX(CASE WHEN measure_name = 'accX' THEN measure_value::double END) "accX",
+     MAX(CASE WHEN measure_name = 'accY' THEN measure_value::double END) "accY",
+     MAX(CASE WHEN measure_name = 'accZ' THEN measure_value::double END) "accZ",
+     MAX(CASE WHEN measure_name = 'pitch' THEN measure_value::double END) "pitch",
+     MAX(CASE WHEN measure_name = 'roll' THEN measure_value::double END) "roll",
+     MAX(CASE WHEN measure_name = 'yaw' THEN measure_value::double END) "yaw"
+FROM "IMU"."data"
 GROUP BY 1
-ORDER BY time DESC 
-LIMIT 10 
+ORDER BY time DESC
+LIMIT 10
